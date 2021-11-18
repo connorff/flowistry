d48034524083f3e0262ecf33f68bd96fe3202685
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-11-17 19:44:22 -0600

    Support unit and install tests

--- ide/src/tests/runTests.ts
+++ ide/src/tests/runTests.ts
@@ -1,8 +1,12 @@
 import path from 'path';
 import { runTests } from 'vscode-test';
+import minimist from 'minimist';
 import { MOCK_PROJECT_DIRECTORY, MOCK_PROJECT_FILES } from './unit/util/constants';
 
 async function main() {
+    const args = minimist(process.argv.slice(2));
+    const isInstall = args._.includes('install');
+
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
@@ -10,14 +14,15 @@ async function main() {
     const launchArgs = ["--disable-extensions", MOCK_PROJECT_DIRECTORY, ...Object.values(MOCK_PROJECT_FILES)];
 
     // All test suites (either unit tests or integration tests) should be in subfolders.
-    const extensionTestsPath = path.resolve(__dirname, './unit/index');
+    const unitTestsPath = path.resolve(__dirname, './unit/index');
+    const installTestsPath = path.resolve(__dirname, './install/index');
 
     // Run tests using the latest stable release of VSCode
     await runTests({
         version: 'stable',
         launchArgs,
         extensionDevelopmentPath,
-        extensionTestsPath
+        extensionTestsPath: isInstall ? installTestsPath : unitTestsPath,
     });
 }
 
