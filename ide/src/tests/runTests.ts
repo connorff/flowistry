62df38f35cd12c976a3a698b3eeeed7c07f4dc54
Connor Fogarty <connorflynnfogarty@gmail.com>
2021-11-18 08:36:16 -0600

    Add test for installing from release

--- ide/src/tests/runTests.ts
+++ ide/src/tests/runTests.ts
@@ -1,17 +1,26 @@
 import path from 'path';
 import { runTests } from 'vscode-test';
 import minimist from 'minimist';
+import { Mockttp } from 'mockttp';
 import { MOCK_PROJECT_DIRECTORY, MOCK_PROJECT_FILES } from './unit/util/constants';
+import { createProxy, PROXY_PORT } from './install/proxy';
 
 async function main() {
     const args = minimist(process.argv.slice(2));
     const isInstall = args._.includes('install');
+    const zip = args['zip'];
+    let server: Mockttp | undefined;
 
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
     const launchArgs = ["--disable-extensions", MOCK_PROJECT_DIRECTORY, ...Object.values(MOCK_PROJECT_FILES)];
+
+    if (isInstall && zip) {
+        server = await createProxy(zip);
+        launchArgs.push(`--proxy-server=http://localhost:${PROXY_PORT}`, '--ignore-certificate-errors')
+    }
 
     // All test suites (either unit tests or integration tests) should be in subfolders.
     const unitTestsPath = path.resolve(__dirname, './unit/index');
@@ -24,6 +33,8 @@ async function main() {
         extensionDevelopmentPath,
         extensionTestsPath: isInstall ? installTestsPath : unitTestsPath,
     });
+
+    server?.stop();
 }
 
 main().catch(err => {
